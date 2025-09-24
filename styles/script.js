5a1df54a714d424436274c249cf221dca4201fb5
joaoomoura0 <mourajoaovictor325@gmail.com>
2025-09-24 15:21:35 -0300

    slider

--- styles/script.js
+++ styles/script.js
@@ -1,23 +1,15 @@
+let count = 1;
+document.getElementById("radio1").checked = true;
 
-    document.addEventListener("DOMContentLoaded", function () {
-        const btn = document.getElementById("toggleTextBtn");
-        const aboutText = document.getElementById("about-text");
+setInterval( function(){
+    nextImage();
+}, 3500)
 
-        let expanded = false;
+function nextImage(){
+    count++;
+    if(count>5){
+        count = 1;
+    }
 
-        const shortText = "Aqui você encontrará mais informações sobre mim, o que faço e minhas habilidades";
-        const fullText = `Opa! Sou o João, tenho 17 anos e estou finalizando o ensino médio junto com o curso técnico de Análise e Desenvolvimento de Sistemas na Escola SESI. Estou dando os primeiros passos na minha jornada como desenvolvedor, explorando, aprendendo e me desafiando a cada novo projeto.
-        
-Sempre fui fascinado pela parte visual de sites e aplicativos. E foi isso que puxou pro front-end, área que mais me identifico e onde venho focando meus estudos.
-
-Apesar do meu foco atual ser o front-end, tenho muita curiosidade por outras áreas da programação e estou sempre buscando aprender novas linguagens e tecnologias.`;
-
-        btn.addEventListener("click", () => {
-            aboutText.textContent = expanded ? shortText : fullText;
-            btn.textContent = expanded ? "Click" : "Voltar";
-            expanded = !expanded;
-        });
-    });
-
-
-
+    document.getElementById("radio"+count).checked = true;
+}
